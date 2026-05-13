be7d73fb7b52d3489041bdc45cd6fb44c6a6b570
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 16:23:50 +0300

    Getting started

---
+++ Tip Calculator/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    
+    var isPortaitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
+    
+    var body: some View {
+        VStack {
+            if isPortaitPhone {
+                Text("Tip Calculator View (portrait)")
+            } else {
+                Text("Tip Calculator View (landscape)")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
