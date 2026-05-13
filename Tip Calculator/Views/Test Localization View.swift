39d4971a5659dc4d8cab4ed4decb28db42620e9f
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 16:36:49 +0300

    Localization

---
+++ Tip Calculator/Views/Test Localization View.swift
@@ -0,0 +1,29 @@
+//
+//  Test Localization View.swift
+//  Tip Calculator
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+struct Test_Localization_View: View {
+    var body: some View {
+        VStack {
+            Text("Amount")
+            Text("Bill")
+            Text("Choose Tip")
+            Text("Custom")
+            Text("Enter Total")
+            Text("Split")
+            Text("Summary")
+            Text("Tip Calculator")
+            Text("Tip")
+            Text("Total per person")
+        }
+    }
+}
+
+#Preview {
+    Test_Localization_View()
+}
