1d7d4e92eeacb118403f79ae0f96209628d74493
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 17:45:01 +0300

    SelectButtonView

---
+++ Tip Calculator/Views/Choose Tip View/ChooseTipView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseTipView.swift
+//  Tip Calculator
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+struct ChooseTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseTipView()
+}
---
+++ Tip Calculator/Views/Choose Tip View/CustomTipView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTipView.swift
+//  Tip Calculator
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+struct CustomTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTipView()
+}
