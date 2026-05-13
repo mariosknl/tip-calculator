9972dece8482a8a95c6672288584a0a7032c5f62
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 17:54:29 +0300

    CustomTipView

---
+++ Tip Calculator/Views/Choose Tip View/TipPercentage.swift
@@ -0,0 +1,16 @@
+//
+//  TipPercentage.swift
+//  Tip Calculator
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import Foundation
+
+enum TipPercentage: Int {
+    case ten = 10
+    case fifteen = 15
+    case twenty = 20
+    case twentyfive = 25
+    case custom = -1
+}
